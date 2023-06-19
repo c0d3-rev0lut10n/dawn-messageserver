018b9d25e396d48156b34897d230a1c90c67bf5e
Laurenz Werner <werner.la@protonmail.com>
2023-06-19 11:58:15 +0200

    test sethandle

--- tests.sh
+++ tests.sh
@@ -19,6 +19,12 @@
 
 #	THIS SCRIPT PROVIDES AUTOMATIC TESTS FOR THE DAWN MESSAGESERVER
 
+# Error return function
+error() { printf "\033[0;31m%s\n\033[0m" "$*" >&2; }
+
+# info function
+notice() { printf "\033[0;32m%s\n\033[0m" "$*" >&1; }
+
 # cd into the directory this script is located at
 cd ${0%/*}
 
@@ -26,3 +32,7 @@ cd ${0%/*}
 mkdir -p runtime
 rm -R runtime/*
 mkdir runtime/handle
+
+# set a handle
+notice "INFO: testing sethandle"
+curl -v -X POST 'http://localhost:8080/sethandle/1234121212121212121212121212121212121212121212121212121212121212/qwerty?password=password&allow_public_init=false&init_secret=blahblahblahblah' --data-binary "@./test-data/test1" || error "ERROR: sethandle failed"

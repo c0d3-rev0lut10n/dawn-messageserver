04f02299e4a76070475283836a0cef9b3dc3c249
Laurenz Werner <werner.la@protonmail.com>
2023-06-20 12:06:06 +0200

    test addkey and improve overall script

--- tests.sh
+++ tests.sh
@@ -20,10 +20,10 @@
 #	THIS SCRIPT PROVIDES AUTOMATIC TESTS FOR THE DAWN MESSAGESERVER
 
 # Error return function
-error() { printf "\033[0;31m%s\n\033[0m" "$*" >&2; }
+error() { printf "\033[0;31m%s\n\033[0m" "ERROR: $*" >&2; exit; }
 
 # info function
-notice() { printf "\033[0;32m%s\n\033[0m" "$*" >&1; }
+notice() { printf "\033[0;32m%s\n\033[0m" "INFO: $*" >&1; }
 
 # cd into the directory this script is located at
 cd ${0%/*}
@@ -33,6 +33,10 @@ mkdir -p runtime
 rm -R runtime/*
 mkdir runtime/handle
 
-# set a handle
-notice "INFO: testing sethandle"
-curl -v -X POST 'http://localhost:8080/sethandle/1234121212121212121212121212121212121212121212121212121212121212/qwerty?password=password&allow_public_init=false&init_secret=blahblahblahblah' --data-binary "@./test-data/test1" || error "ERROR: sethandle failed"
+# set a handle (private init)
+notice "testing sethandle"
+curl -v -X POST 'http://localhost:8080/sethandle/1234121212121212121212121212121212121212121212121212121212121212/qwerty?password=password&allow_public_init=false&init_secret=blahblahblahblah' --data-binary "@./test-data/test1" || error "sethandle failed"
+
+# add a key to the handle
+notice "adding key to handle"
+curl -v -X POST 'http://localhost:8080/addkey/qwerty?password=password' --data-binary "@./test-data/test2" || error "addkey failed"
